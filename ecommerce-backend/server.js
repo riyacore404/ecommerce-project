7c6e518c05d059105044ff031f4fd7cd24edeced
riyacore404 <rr1182764@gmail.com>
2025-12-17 02:54:12 +0530

    Update server.js

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -43,14 +43,15 @@ app.use('/api/payment-summary', paymentSummaryRoutes);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch-all route to serve index.html for any unmatched routes
-app.get('*', (req, res) => {
-  const indexPath = path.join(__dirname, 'dist', 'index.html');
-  if (fs.existsSync(indexPath)) {
-    res.sendFile(indexPath);
-  } else {
-    res.status(404).send('index.html not found');
-  }
-});
+// !!!!!!!this is CHANGEDD!!!!!!!
+// app.get('*', (req, res) => {
+//   const indexPath = path.join(__dirname, 'dist', 'index.html');
+//   if (fs.existsSync(indexPath)) {
+//     res.sendFile(indexPath);
+//   } else {
+//     res.status(404).send('index.html not found');
+//   }
+// });
 
 // Error handling middleware
 /* eslint-disable no-unused-vars */
